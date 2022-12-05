abf154dfc60de3934da0137598b8307916dc71ea
Alexander <LipatovAlexander93@yandex.ru>
2022-12-05 21:31:36 +0300

    added login.html, register.html and LoginController

---
+++ src/main/java/com/github/mnemalotebya/springbootshoppinglist/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.github.mnemalotebya.springbootshoppinglist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+}
